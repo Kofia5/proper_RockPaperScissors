2eea6da879c700b4bf36b5f48ea9a907a34b0fe8
Kofi Appiah <Kofia5@gmail.com>
2011-07-21 15:09:07 -0400

    ADD initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProperRockPaperScissors::Application.config.secret_token = '56260887dcc82a799b8db25c347e8cd2210f5b07b6ab74a10973fa5ac42649667894788e0eec5268dba6300ece5faf946e98f3d727c236bccdd327af364316ca'
